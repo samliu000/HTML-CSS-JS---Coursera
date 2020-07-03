fd48ad2b38fc5671eb6ab80e6ec59fce520b054b
samliu000 <44206874+samliu000@users.noreply.github.com>
2020-07-03 10:06:56 -0700

    JS

--- site3-js/js/script.js
+++ site3-js/js/script.js
@@ -2,3 +2,24 @@ var string = "Hello";
 
 string = string+" World";
 console.log(string+"!");
+
+
+console.log((5+4) / 3);
+console.log(undefined/5);
+test1(2);
+function test1 (a) {
+	console.log(a/5);
+}
+test1();
+
+x='4';
+y=4;
+if(x==y) {
+	console.log("Non-strict: x='4' is equal to y=4");
+}
+if(x===y) {
+	console.log("Strict: x='4' is equal to y=4");
+}
+else {
+	console.log("Not equal");
+}
