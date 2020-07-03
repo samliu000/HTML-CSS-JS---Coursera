7adb98c82a20787683fc160ac4792afd55725d8f
samliu000 <44206874+samliu000@users.noreply.github.com>
2020-07-03 11:09:02 -0700

    JS

--- site3-js/js/script.js
+++ site3-js/js/script.js
@@ -40,3 +40,18 @@ function orderChickenWith(sideDish) {
 
 orderChickenWith("noodles");
 orderChickenWith();
+
+// Object creation
+var company = new Object();
+company.name = "Facebook";
+console.log(company);
+company.ceo = new Object();
+company.ceo.firstName = "Mark";
+company.ceo.favColor = "blue";
+console.log(company);
+console.log("Company CEO name is: " + company.ceo.firstName);
+console.log(company["name"]);
+
+var stockPropName = "stock of company";
+company[stockPropName] = 110;
+console.log("Stock price is: " + company["stock of company"]);
