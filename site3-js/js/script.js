2f38df96377a32cc267ee1b632b27a4b7fed49b0
samliu000 <samuel.liu1004@gmail.com>
2020-07-04 12:57:29 -0700

    JS

--- site3-js/js/script.js
+++ site3-js/js/script.js
@@ -55,3 +55,62 @@ console.log(company["name"]);
 var stockPropName = "stock of company";
 company[stockPropName] = 110;
 console.log("Stock price is: " + company["stock of company"]);
+
+// Object creation with object literal
+var facebook = {
+	name: "Facebook",
+	ceo: {
+		firstName: "Mark",
+		favColor: "blue"
+	},
+	"stock of company": 110
+};
+console.log(facebook);
+
+// Functions explained
+function multiply(x, y) {
+	return x * y;
+}
+console.log(multiply(5,3));
+multiply.version = "v.1.0.0";
+console.log(multiply.version);
+
+// Function factory
+function makeMultiplier(multiplier) {
+	var myFunc = function(x) {
+		return multiplier * x;
+	};
+	return myFunc;
+}
+
+var multiplyBy3 = makeMultiplier(3);
+console.log(multiplyBy3(10));
+var doubleAll = makeMultiplier(2);
+console.log(doubleAll(100));
+
+// Passing functions as arguments
+function doOperationOn(x, operation) {
+	return operation(x);
+}
+var result = doOperationOn(5, multiplyBy3);
+console.log(result);
+result = doOperationOn(100, doubleAll);
+console.log(result);
+
+// pass by value
+function changePrimitive(primValue) {
+	console.log("in changePrimitive...");
+	console.log("before:");
+	console.log(primValue);
+
+	primValue = 5;
+	console.log("after:");
+	console.log(primValue);
+}
+
+var value = 7;
+changePrimitive(value);
+console.log("after changePrimitive, orig value:");
+console.log(value);
+
+
