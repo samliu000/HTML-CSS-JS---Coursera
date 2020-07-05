f13436993defd78fbc7a52a6268c9a5fd6a2d1b6
samliu000 <samuel.liu1004@gmail.com>
2020-07-05 12:47:48 -0700

    JS arrays

--- site3-js/js/script.js
+++ site3-js/js/script.js
@@ -134,3 +134,61 @@ Circle.prototype.getArea =
 var myCircle = new Circle(10);
 console.log(myCircle);
 
+// Object literals and "this"
+var literalCircle = {
+	radius: 10,
+	getArea: function() {
+		var self = this;
+		console.log(this);
+
+		var increaseRadius = function() {
+			self.radius = 20;
+		};
+		increaseRadius();
+		console.log(this.radius);
+
+		return Math.PI * Math.pow(this.radius, 2);
+	}
+}
+console.log(literalCircle.getArea());
+
+// Arrays
+var array = new Array();
+array[0] = "Yaakov";
+array[1] = 2;
+array[2] = function (name) {
+	console.log("Hello " + name);
+};
+array[3] = {course: "HTML, CSS, & JS"};
+console.log(array);
+console.log(array[1]);
+array[2]("Sam");
+array[2](array[0]);
+console.log(array[3].course);
+
+// Short hand array creation
+var names = ["Sam", "Dave", "John"];
+for(var i = 0; i < names.length; i++) {
+	console.log(names[i]);
+}
+
+names[100] = "Jim";
+
+var names2 = ["Sam", "Dave", "John"];
+var myObj = {
+	name: "Sam",
+	course: "HTML/CSS/JS",
+	platform: "Coursera"
+};
+for (var prop in myObj) {
+	console.log(prop + ": " + myObj[prop]);
+}
+
+for(var name in names2) {
+	console.log("Hello " + names2[name]);
+}
+names2.greeting="Hi!";
+for(var name in names2) {
+	console.log("Hello " + names2[name]);
+}
+console.log(names2);
