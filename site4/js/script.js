803c16f20b6304fb2d5fee41b0144011fad53f5e
samliu000 <44206874+samliu000@users.noreply.github.com>
2020-07-08 19:49:06 -0700

    Handling Events

--- site4/js/script.js
+++ site4/js/script.js
@@ -1,18 +1,34 @@
+// Event handling
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+
+    function sayHello(event) {
+      this.textContent = "Said it";
+      // body...
+      var name = document.getElementById("name").value;
+      var message = "<h2>Hello " + name + "!</h2>";
+
+      // document.getElementById("content").textContent = message;
+      document.getElementById("content").innerHTML = message;
+
+      if(name === "student") {
+        var title = document.querySelector("#title").textContent;
+        title += " & Lovin' it!";
+        document.querySelector("h1").textContent = title;
+      }
+    }
+
+    // document.querySelector("button").onclick = sayHello;
+    // Unobstrusive event binding
+    document.querySelector("button").addEventListener("click", sayHello);
+
+
+  }
+);
 // DOM manipulation
 // console.log(document.getElementById("title"));
 // console.log(document instanceof HTMLDocument);
 
-function sayHello() {
-  // body...
-    var name = document.getElementById("name").value;
-    var message = "<h2>Hello " + name + "!</h2>";
 
-    // document.getElementById("content").textContent = message;
-    document.getElementById("content").innerHTML = message;
 
-    if(name === "student") {
-      var title = document.querySelector("#title").textContent;
-      title += " & Lovin' it!";
-      document.querySelector("#title").textContent = title;
-    }
-}
+
