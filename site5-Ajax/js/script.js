26c5d3d948ac8c286b334d520287a4aa1307dc53
samliu000 <samuel.liu1004@gmail.com>
2020-07-12 15:20:28 -0700

    JS

--- site5-Ajax/js/script.js
+++ site5-Ajax/js/script.js
@@ -8,15 +8,23 @@ document.addEventListener("DOMContentLoaded",
         
         // Call server to get the name
         $ajaxUtils
-          .sendGetRequest("data/name.txt", 
-            function (request) {
-              var name = request.responseText;
+          .sendGetRequest("data/name.json", 
+            function (res) {
+              var message = 
+                res.firstName + " " + res.lastName
+              if (res.likesChineseFood) {
+                message += " likes Chinese food";
+              }
+              else {
+                message += " doesn't like Chinese food";
+              }
+              message += " and uses ";
+              message += res.numberOfDisplays + 1;
+              message += " displays for coding.";
 
               document.querySelector("#content")
-                .innerHTML = "<h2>Hello " + name + "!</h2>";
+                .innerHTML = "<h2>" + message + "</h2>";
             });
-
-        
       });
   }
 );
